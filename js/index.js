29af14b69ff4bcbba20cf16e8090f7cf03e72a57
Wide97 <mwidesott@gmail.com>
2024-11-15 11:58:46 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,32 @@
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:3001/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token);
+            window.location.href = 'dashboard.html';
+        } else {
+            document.getElementById('message').textContent = 'Email o password non validi!';
+        }
+    } catch (error) {
+        console.error('Errore durante il login:', error);
+        document.getElementById('message').textContent = 'Errore di connessione!';
+    }
+});
+
+
+document.getElementById('registerButton').addEventListener('click', function () {
+    window.location.href = 'registrazione.html';
+});
